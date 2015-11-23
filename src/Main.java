39c353bbdbb6f70d542ba9f6f149817270e225aa
vandergoes <mauritsvandergoes@gmail.com>
2015-11-23 23:41:48 +0100

    Added comments, contributions, ratings & part-ups attributes; Removed double relation -Neo

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ import com.mongodb.client.MongoDatabase;
 import java.io.File;
 import java.util.*;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.bson.Document;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.unsafe.impl.batchimport.cache.idmapping.string.Radix;
 
 public class Main {
 
@@ -68,7 +70,7 @@ public class Main {
                 }
             }
         }
-        System.out.println(users.size() + " users imported in Neo4j.");
+        System.out.println(users.size() + " users imported into Neo4j.");
 
         //Networks
         List<Document> networks = MDB.getCollection("Networks").find().into(new ArrayList<Document>());
@@ -110,14 +112,16 @@ public class Main {
             List uppers = (List) network.get("uppers");
             if (uppers != null) {
                 for (int i = 0; i < uppers.size(); i++) {
-                    String query = "MERGE (u:User {_id: '" + uppers.get(i) + "'})" +
-                            "MERGE (n:Network {_id:'" + _id + "'})" +
-                            "CREATE UNIQUE (u)-[:MEMBER_OF]->(n)";
-                    graphDb.execute(query);
+                    if (uppers.get(i) != admin_id) {
+                        String query = "MERGE (u:User {_id: '" + uppers.get(i) + "'})" +
+                                "MERGE (n:Network {_id:'" + _id + "'})" +
+                                "CREATE UNIQUE (u)-[:MEMBER_OF]->(n)";
+                        graphDb.execute(query);
+                    }
                 }
             }
         }
-        System.out.println(networks.size() + " networks imported in Neo4j.");
+        System.out.println(networks.size() + " networks imported into Neo4j.");
 
         //partups
         List<Document> partups = MDB.getCollection("Partups").find().into(new ArrayList<Document>());
@@ -126,6 +130,11 @@ public class Main {
             String name_raw = (String) partup.get("name");
             String name = name_raw.replace("'", "");
             String creator_id = (String) partup.get("creator_id");
+            String purpose = (String) partup.get("purpose");
+            String language = (String) partup.get("language");
+            int privacy_type = (int) partup.get("privacy_type");
+            String type_partup = (String) partup.get("type");
+            String phase = (String) partup.get("phase");
             Document location = (Document) partup.get("location");
             if (location != null) {
                 String place_id = (String) location.get("place_id");
@@ -139,8 +148,13 @@ public class Main {
                             "MERGE (u:User {_id: '" + creator_id + "'})" +
                             "MERGE (t:Team {_id:'" + _id + "'})" +
                             "SET t.name='" + name + "'," +
-                            "t.tags=[]" +
-                            "CREATE UNIQUE (u)-[:CREATOR_OF]->(t)," +
+                            "t.tags=[]," +
+                            "t.purpose='"+ purpose +"'," +
+                            "t.language='" + language + "'," +
+                            "t.privacy_type=" + privacy_type + "," +
+                            "t.type='"+ type_partup + "'," +
+                            "t.phase='" + phase + "'" +
+                            "CREATE UNIQUE (u)-[:CREATOR_OF {comments:0, contributions:0}]->(t)," +
                             "(t)-[:LOCATED_IN]->(ci)," +
                             "(ci)-[:LOCATED_IN]->(co)";
                     graphDb.execute(user_query);
@@ -148,24 +162,34 @@ public class Main {
                     String user_query = "MERGE (u:User {_id: '" + creator_id + "'})" +
                             "MERGE (t:Team {_id:'" + _id + "'})" +
                             "SET t.name='" + name + "'," +
-                            "t.tags=[]" +
-                            "CREATE UNIQUE (u)-[:CREATOR_OF]->(t)";
+                            "t.tags=[]," +
+                            "t.purpose='"+ purpose +"'," +
+                            "t.language='" + language + "'," +
+                            "t.privacy_type=" + privacy_type + "," +
+                            "t.type='"+ type_partup + "'," +
+                            "t.phase='" + phase + "'" +
+                            "CREATE UNIQUE (u)-[:CREATOR_OF {comments:0, contributions:0, pageViews:0, ratings:[]}]->(t)";
                     graphDb.execute(user_query);
                 }
             } else{
                 String user_query = "MERGE (u:User {_id: '" + creator_id + "'})" +
                         "MERGE (t:Team {_id:'" + _id + "'})" +
                         "SET t.name='" + name + "'," +
-                        "t.tags=[]" +
-                        "CREATE UNIQUE (u)-[:CREATOR_OF]->(t)";
+                        "t.tags=[]," +
+                        "t.purpose='"+ purpose +"'," +
+                        "t.language='" + language + "'," +
+                        "t.privacy_type=" + privacy_type + "," +
+                        "t.type='"+ type_partup + "'," +
+                        "t.phase='" + phase + "'" +
+                        "CREATE UNIQUE (u)-[:CREATOR_OF {comments:0, contributions:0, pageViews:0, ratings:[]}]->(t)";
                 graphDb.execute(user_query);
             }
             List partners = (List) partup.get("uppers");
-            if (partners != null) {
-                for (int i = 0; i < partners.size(); i++) {
+            for (int i = 0; i < partners.size(); i++) {
+                if (!partners.get(i).equals(creator_id)){
                     String query = "MERGE (u:User {_id: '" + partners.get(i) + "'})" +
                             "MERGE (t:Team {_id:'" + _id + "'})" +
-                            "CREATE UNIQUE (u)-[:PARTNER_IN]->(t)";
+                            "CREATE UNIQUE (u)-[:PARTNER_IN {comments:0, contributions:0, pageViews:0, ratings:[]}]->(t)";
                     graphDb.execute(query);
                 }
             }
@@ -174,7 +198,7 @@ public class Main {
                 for (int i = 0; i < supporters.size(); i++) {
                     String query = "MERGE (u:User {_id: '" + supporters.get(i) + "'})" +
                             "MERGE (t:Team {_id:'" + _id + "'})" +
-                            "CREATE UNIQUE (u)-[:SUPPORTER_IN]->(t)";
+                            "CREATE UNIQUE (u)-[:SUPPORTER_IN {comments:0, pageViews:0}]->(t)";
                     graphDb.execute(query);
                 }
             }
@@ -187,7 +211,53 @@ public class Main {
                 }
             }
         }
-        System.out.println(partups.size() + " teams imported in Neo4j.");
+        System.out.println(partups.size() + " teams imported into Neo4j.");
+
+        //Comments
+        List<Document> comments = MDB.getCollection("Updates").find().into(new ArrayList<Document>());
+        int count_comments = 0;
+        for (Document comment : comments) {
+            String type = (String) comment.get("type");
+            if (type.equals("partups_message_added") || type.equals("partups_activities_comments_added") || type.equals("partups_contributions_comments_added")){
+                String upper_id = (String) comment.get("upper_id");
+                String partup_id = (String) comment.get("partup_id");
+                String query = "MATCH (u:User {_id: '" + upper_id + "'})-[r]->(t:Team {_id: '" + partup_id + "'})" +
+                        "SET r.comments=r.comments+1";
+                graphDb.execute(query);
+                count_comments = count_comments + 1;
+            }
+        }
+        System.out.println(count_comments + " comments imported into Neo4j.");
+
+        //Contributions
+        List<Document> contributions = MDB.getCollection("Contributions").find().into(new ArrayList<Document>());
+        int count_contributions = 0;
+        for (Document contribution : contributions) {
+            Boolean verified = (Boolean) contribution.get("verified");
+            if (verified) {
+                String upper_id = (String) contribution.get("upper_id");
+                String partup_id = (String) contribution.get("partup_id");
+                String query = "MATCH (u:User {_id: '" + upper_id + "'})-[r]->(t:Team {_id: '" + partup_id + "'})" +
+                        "SET r.contributions=r.contributions+1";
+                graphDb.execute(query);
+                count_contributions = count_contributions + 1;
+            }
+        }
+        System.out.println(count_contributions + " contributions imported into Neo4j.");
+
+        //Ratings
+        List<Document> ratings = MDB.getCollection("Ratings").find().into(new ArrayList<Document>());
+        for (Document rating : ratings) {
+            String rated_upper_id = (String) rating.get("rated_upper_id");
+            String partup_id = (String) rating.get("partup_id");
+            int rating_value = (int) rating.get("rating");
+            String query = "MATCH (u:User {_id: '" + rated_upper_id + "'})-[r]->(t:Team {_id: '" + partup_id + "'})" +
+                  "SET r.ratings=r.ratings+["+ rating_value + "]";
+            graphDb.execute(query);
+        }
+        System.out.println(ratings.size() + " ratings imported into Neo4j.");
+
+        System.out.println("Happy Hunting!");
     }
 
     private static void registerShutdownHook( final GraphDatabaseService graphDb )
